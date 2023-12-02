c7b0db7d0efadaf1dae5af96d916c78ff4b5ed20
prac_13 <pradeep@bytebeam.io>
2023-12-02 16:56:35 +0530

    both Rx and Tx working version0

--- esp_spi_test.ino/esp_spi_test.ino.ino
+++ esp_spi_test.ino/esp_spi_test.ino.ino
@@ -14,6 +14,8 @@
 #define MSG_ABSENT 0x32
 #define READ_MSG 0x03
 #define ABORT_TRANSACTION 0x02
+#define WRITE_CAN 0xAB
+#define  NO_CAN_WRITE 0xAC
 
 void send_read_command(void);uint8_t flag = 0;
 uint8_t test_snd_buff = {0x03};
@@ -21,6 +23,11 @@ uint8_t test_rcv_buff;
 int j = 0;
 uint32_t canId = 0;
 static int can_msg_count = 0;
+uint8_t can_present = 0;
+
+uint8_t CAN_DATA[12];
+
+int count = 0;
 
 void setup() {
   // Initialize SPI communication
@@ -34,20 +41,43 @@ void setup() {
 }
 void loop()
 {
+  count++;
+  if(count == 1000)
+  {
+    count = 0;
+    can_present = 1;
+  }
+  digitalWrite(SS_PIN, LOW);
   check_and_receive_can();  
+  digitalWrite(SS_PIN, HIGH);
 //  delayMicroseconds(500);
   delay(1);
 }
 
 void check_and_receive_can()
 {
-  digitalWrite(SS_PIN, LOW);
+  
   test_snd_buff = CAN_MSG_STATUS;
   test_rcv_buff = SPI.transfer(test_snd_buff);        //It will send 0x03(Query)and it will receive 0x33(resp)
 //  Serial.println(test_rcv_buff, HEX); 
   if(test_rcv_buff == MSG_PRESENT)
   {
     can_msg_count++;
+    if(can_present == 1)
+    {
+      test_snd_buff = WRITE_CAN;
+      test_rcv_buff = SPI.transfer(test_snd_buff);
+      for(uint8_t x = 1;x < 13;x++)
+      {
+        test_rcv_buff = SPI.transfer(x);
+      }
+      can_present = 0;
+    }
+    else
+    {
+      test_snd_buff = NO_CAN_WRITE;
+      test_rcv_buff = SPI.transfer(test_snd_buff);
+    }
     test_snd_buff = READ_MSG;
     test_rcv_buff = SPI.transfer(test_snd_buff);
     send_read_command();
@@ -55,9 +85,23 @@ void check_and_receive_can()
   else
   {
     test_snd_buff = ABORT_TRANSACTION;
+    if(can_present == 1)
+    {
+      test_snd_buff = WRITE_CAN;
+      test_rcv_buff = SPI.transfer(test_snd_buff);
+      for(uint8_t x = 0;x < 12;x++)
+      {
+        test_rcv_buff = SPI.transfer(x);
+      }
+      can_present = 0;
+    }
+    else
+    {
+      test_snd_buff = NO_CAN_WRITE;
+      test_rcv_buff = SPI.transfer(test_snd_buff);
+    }
 //    test_rcv_buff = SPI.transfer(test_snd_buff);
   }
-  digitalWrite(SS_PIN, HIGH);
 }
 
 void send_read_command(void)
@@ -69,27 +113,26 @@ void send_read_command(void)
   {
     receivedData[i] = SPI.transfer(dataToSend[i]);
   }
-//  Serial.println("Sequence number is:-");
-//  Serial.println("Received CAN ID is:-");
-//  canId |= ((uint32_t)receivedData[1] << 24);
-//  canId |= ((uint32_t)receivedData[2] << 16);
-//  canId |= ((uint32_t)receivedData[3] << 8);
-//  canId |= receivedData[4];
+  Serial.println("Received CAN ID is:-");
+  canId |= ((uint32_t)receivedData[1] << 24);
+  canId |= ((uint32_t)receivedData[2] << 16);
+  canId |= ((uint32_t)receivedData[3] << 8);
+  canId |= receivedData[4];
   
-//  Serial.println(canId, HEX);
+  Serial.println(canId, HEX);
   Serial.println(can_msg_count);
 //  Serial.println("Length of message is:-");
 //  Serial.println(receivedData[13], HEX);
   
   canId = 0;
   
-//  Serial.println("Received CAN data bytes are:-");
+  Serial.println("Received CAN data bytes are:-");
   for(j = 5;j < 13;j++)
   {
     sum += receivedData[j];
-//    Serial.print(receivedData[j], HEX);
-//    if(j != 12)
-//      erial.print(",");
+    Serial.print(receivedData[j], HEX);
+    if(j != 12)
+      Serial.print(",");
   }
   if(sum != 36)    
     Serial.println("Fault");
